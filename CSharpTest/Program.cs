dd34d4ee1fd836ffb3c3d303f9ef9236b3b85f89
Mikkel Storgaard Knudsen <mikkelstorgaard@gmail.com>
2018-09-04 08:35:38 +0200

    Don't leave out the C# example

---
+++ CSharpTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Matrix;
+namespace CSharpTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = 5;
+            var m = 7;
+            var p = 6;
+            
+            var matrix = new Matrix.Matrix(args);
+
+            var A = matrix.replicateRows(n, m);
+            var B = matrix.replicateRows(m, p);
+            var AB = matrix.multiply(A, B);
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < p; j++)
+                {
+                    Console.Write("{0}\t", AB.Item1[(i*p)+j]);
+                }
+                Console.Write("\n");
+            }
+            Environment.Exit(0);
+        }
+    }
+}
